ef42e71f7baaf4e3f1b1b1e802759b23905f37dd
Carlos Zambrano <czam01@gmail.com>
2020-12-12 22:59:57 -0500

    add athena dag

--- plugins/athena_plugin.py
+++ plugins/athena_plugin.py
@@ -2,10 +2,10 @@ from airflow.plugins_manager import AirflowPlugin
 from hooks.aws_athena_hook import AWSAthenaHook
 from operators.aws_athena_operator import AWSAthenaOperator
 
-class aws_athena_operator(SlackWebhookOperator):
+class aws_athena_operator(AWSAthenaOperator):
   pass
 
-class aws_athena_hook(SlackWebhookHook):
+class aws_athena_hook(AWSAthenaHook):
   pass
 
 class slack_plugin(AirflowPlugin):
